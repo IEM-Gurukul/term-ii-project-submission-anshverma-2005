2f8640d0a177f518decde95c3f9bc70e6c0afdc3
anshverma-2005 <anshvermachiku@gmail.com>
2026-03-29 01:21:05 +0530

    Added Player class

---
+++ src/Player.java
@@ -0,0 +1,26 @@
+class Player {
+    private String name;
+    private int matches;
+    private int runs;
+    private int wickets;
+
+    public Player(String name, int matches, int runs, int wickets) {
+        this.name = name;
+        this.matches = matches;
+        this.runs = runs;
+        this.wickets = wickets;
+    }
+
+    public double getBattingAverage() {
+        if (matches == 0) return 0;
+        return (double) runs / matches;
+    }
+
+    public void displayStats() {
+        System.out.println("Name: " + name);
+        System.out.println("Matches: " + matches);
+        System.out.println("Runs: " + runs);
+        System.out.println("Wickets: " + wickets);
+        System.out.println("Average: " + getBattingAverage());
+    }
+}
