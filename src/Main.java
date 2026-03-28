3bfd7d9452447895c4e08ea7e4b722ac8e00644f
anshverma-2005 <anshvermachiku@gmail.com>
2026-03-29 01:36:49 +0530

    Updated Main class with more players

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Player p1 = new Player("Virat Kohli", 250, 12000, 5);
+        Player p2 = new Player("MS Dhoni", 300, 10500, 1);
+        Player p3 = new Player("Rohit Sharma", 240, 9800, 8);
+
+        System.out.println("Player 1 Stats:");
+        p1.displayStats();
+
+        System.out.println("\nPlayer 2 Stats:");
+        p2.displayStats();
+
+        System.out.println("\nPlayer 3 Stats:");
+        p3.displayStats();
+    }
+}
